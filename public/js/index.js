8c34f99f0cf0bf2329e1efa47be947029d189a9e
Zosia Fabijanska <zosia@good.work>
2018-07-21 15:34:55 +1000

    version two of cornerstone website

--- public/js/index.js
+++ public/js/index.js
@@ -1,73 +1,38 @@
-//Google Maps API
-function initMap() {
-    var northcote = { lat: -37.778, lng: 145.01 };
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 14,
-        center: northcote
-    });
-    var marker = new google.maps.Marker({
-        position: northcote,
-        map: map
-    });
-}
-
-
 $(document).ready(() => {
-    
-    //Element cache
+
+    //Cache the body element
     const $body = $("html, body");
-    const $fadeIn = $('.fade-in');
-    const $fadeInText = $('.fade-in-text');
-    
+
+    //Function to scroll to page sections
     const slowScrollTo = section => {
         $body.animate(
             {
                 scrollTop: $(section).offset().top
             },
-            800
+            800,
         );
     };
 
     // Set Event Handlers
-    $(".scroll-icon").click(() => {
-        slowScrollTo("#menu-section");
-    });
     
     $("#to-gallery").click(() => {
+        event.preventDefault();
         slowScrollTo("#gallery");
     });
     
-    $("#to-contact").click(() => {
-        slowScrollTo("#location");
-    });
-    
-    $("#to-location").click(() => {
-        slowScrollTo("#location");
+    $("#to-aboutus").click(() => {
+        event.preventDefault();
+        slowScrollTo("#about-us");
     });
 
-    //Fade In Scroll
-    // Set hidden elements
-    $fadeIn.css({
-        opacity: 0
-    })
-    $fadeInText.css({
-        opacity:0
-    })
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll(() => {
-        /* Check the location of each desired element */
-        const bottom_of_object = $fadeIn.offset().top + ($fadeIn.outerHeight() / 2);
-        const bottom_of_window = $(window).scrollTop() + $(window).height();
-        /* If the object is completely visible in the window, fade it it */
-        if (bottom_of_window > bottom_of_object) {
-            $fadeIn.animate({ opacity: "1" }, 900);
-            $fadeInText.animate({opacity:"1"}, 1200);
-        };
-        
-        const wScroll = $(this).scrollTop();
-        $(".corner-logo-lg").css({
-            transform: "translate(0px, " + wScroll / 5 + "%)"
-        });
+    //Scroll Reveal
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.scroll-reveal', {
+        distance: '100px',
+        duration: 800,
+        delay: 300,
+        scale: 1,
     });
 });
